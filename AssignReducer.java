ec06d449a6aef5bc5d2c095c3d749c5135dfc2e6
ShruthiElan <shruelan@gmail.com>
2017-04-11 09:21:47 +0530

    Add files via upload

---
+++ AssignReducer.java
@@ -0,0 +1,35 @@
+import java.io.IOException;
+
+
+import org.apache.hadoop.io.IntWritable;
+
+import org.apache.hadoop.io.Text;
+
+import org.apache.hadoop.mapreduce.Reducer;
+
+
+public class AssignReducer extends Reducer<Text, IntWritable, Text, IntWritable>
+{
+
+IntWritable count = new IntWritable();
+	
+	
+public void reduce(Text key, Iterable<IntWritable> values,Context context) throws IOException, InterruptedException
+	
+{
+		
+int s = 0;
+		
+for (IntWritable value : values)
+ {
+			
+s=s+value.get();
+
+}
+		
+count.set(s);
+
+context.write(key, count);
+	
+}
+}
